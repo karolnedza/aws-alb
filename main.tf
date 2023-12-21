b4dd0f65e0d1d43f89e553b4622caabe12c0166c
Karol Nedza <karol@Karols-MacBook-Pro.local>
2023-12-21 20:09:59 +0100

    removing one ALB from WAF

--- main.tf
+++ main.tf
@@ -224,10 +224,10 @@ resource "aws_security_group" "allow_tls" {
 
 
 
-resource "aws_wafv2_web_acl_association" "example2" {
-   resource_arn = aws_lb.test2.arn
-   web_acl_arn  = aws_wafv2_web_acl.example2.arn
-}
+# resource "aws_wafv2_web_acl_association" "example2" {
+#    resource_arn = aws_lb.test2.arn
+#    web_acl_arn  = aws_wafv2_web_acl.example2.arn
+# }
 
 #### 
 
